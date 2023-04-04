5bedc5b42444b7b7e7fddd3f17626b1b4d8a3f8b
erickleclerc <107086802+erickleclerc@users.noreply.github.com>
2023-04-04 14:41:26 -0700

    Added Spectator Cam/Medieval Template

---
+++ Assets/Ragnarok/Scripts/SpectatorCamera.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SpectatorCamera : MonoBehaviour
+{
+    public Transform target;
+    public float translateSpeed = 6f;
+    public float rotateSpeed = 8f;
+
+
+    //CHANGE SPECTATOR CAMERA FIELD OF VIEW TO 90
+
+    //so that all the logic has finished before executing
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position;
+        Quaternion targetRotation = target.rotation;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+    }
+}
